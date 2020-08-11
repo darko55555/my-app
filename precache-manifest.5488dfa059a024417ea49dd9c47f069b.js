41431e46c3af9f0111033246b19fd608d917ee2a
darko55555 <darko.j.dujmovic@gmail.com>
2020-08-11 15:10:22 +0200

    Updates

---
+++ precache-manifest.5488dfa059a024417ea49dd9c47f069b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a432e7359a3312b9eccfcbbbe2bfb50f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b06bbde20729dc59853a",
+    "url": "/my-app/static/css/main.2325b611.chunk.css"
+  },
+  {
+    "revision": "2ce22bf69ca2369071da",
+    "url": "/my-app/static/js/2.247f53e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.247f53e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06bbde20729dc59853a",
+    "url": "/my-app/static/js/main.230c75d5.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
