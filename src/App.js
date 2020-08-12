7f74b122a87cc15d3befe961371457b2d15a8d11
darko55555 <darko.j.dujmovic@gmail.com>
2020-08-12 12:53:13 +0200

    Changed how button behaves when long text

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function Todo({ todo, index, completeTodo, removeTodo }) {
     <div
       className="todo"
       style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
-      <label class=".text-xl">{todo.text}</label>
+      <label class=".text-xl .break-words">{todo.text}</label>
 
-      <div>
+      <div class="md:flex-shrink-0">
         <button
         class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
         onClick={() => completeTodo(index)}> {todo.isCompleted ? "Uncomplete" : "Complete"}</button>
